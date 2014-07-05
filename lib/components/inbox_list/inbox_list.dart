e071d7b500366c7d733828147ccef4d20c29f526
David Notik <dave@woven.org>
2014-07-05 05:24:44 -0400

    Ascending inbox list

--- lib/components/inbox_list/inbox_list.dart
+++ lib/components/inbox_list/inbox_list.dart
@@ -5,12 +5,14 @@ export 'package:polymer/init.dart';
 @CustomTag('inbox-list')
 class InboxList extends PolymerElement with Observable {
   @observable List items = toObservable([]);
+  @observable List reverse;
 
   var f = new db.Firebase('https://luminous-fire-4671.firebaseio.com');
 
   getItems() {
     f.onChildAdded.forEach((e) {
       items.add(e.snapshot.val());
+      reverse = toObservable(items.reversed);
       print(e.snapshot.val());
     });
   }
