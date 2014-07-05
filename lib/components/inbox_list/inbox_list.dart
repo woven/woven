d59d4b2172cfd8da4df6f46260448466126a0d69
David Notik <dave@woven.org>
2014-07-05 05:05:36 -0400

    InboxList component now gets list from db

---
+++ lib/components/inbox_list/inbox_list.dart
@@ -0,0 +1,27 @@
+import 'package:polymer/polymer.dart';
+import 'package:firebase/firebase.dart' as db;
+export 'package:polymer/init.dart';
+
+@CustomTag('inbox-list')
+class InboxList extends PolymerElement with Observable {
+  @observable List items = toObservable([]);
+
+  var f = new db.Firebase('https://luminous-fire-4671.firebaseio.com');
+
+  getItems() {
+    f.onChildAdded.forEach((e) {
+      items.add(e.snapshot.val());
+      print(e.snapshot.val());
+    });
+  }
+
+  InboxList.created() : super.created() {
+    getItems();
+    //items = items.reversed.toList();
+  }
+
+  attached() => print("+b");
+  detached() => print("-b");
+
+}
+
--- web/index.dart
+++ web/index.dart
@@ -1,28 +1,30 @@
 import 'package:polymer/polymer.dart';
 import 'dart:html';
 import 'dart:async';
-import 'package:firebase/firebase.dart';
-import 'package:../../lib/components/inbox_list/inbox_list.dart';
+import '../lib/components/inbox_list/inbox_list.dart';
+import 'package:firebase/firebase.dart' as db;
 
-var f = new Firebase('https://luminous-fire-4671.firebaseio.com');
+final f = new db.Firebase('https://luminous-fire-4671.firebaseio.com');
 
 void main() {
   querySelector('#doButton').onClick.listen(doStuff);
 
   var doForm = querySelector('#doForm');
+
   doForm.onSubmit.listen((e) {
     print("Form was submitted...");
   });
 
-  List inboxItems = new List();
-  inboxItems = ['Cool','Awesome','Rad as heck', 'Lorem ipsum dwewedolot sit amet'];
+  //var inboxList = new InboxList();
+  //List inboxItems = InboxList.items;
+  //inboxItems = ['Cool','Awesome','Rad as heck', 'Lorem ipsum dwewedolot sit amet'];
 
 //  f.onChildAdded.forEach((e) {
 //    inboxItems.add(e.snapshot.val());
 //  });
 
-  print("Outside of initPolymer: $inboxItems");
-  //print("Outside of initPolymer: $items");
+  //print("Outside of initPolymer: $inboxItems");
+  //print("Outside of initPolymer: $InboxList.items");
 
   // Placeholder for when we want to do stuff after Polymer elements fully loaded
   //TODO: What's a cleaner way to organize this?
@@ -30,35 +32,30 @@ void main() {
     // code here works most of the time
     Polymer.onReady.then((_) {
       // some things must wait until onReady callback is called
-
-      f.onChildAdded.forEach((e) {
-        //print(e.snapshot.val());
-        inboxItems.add(e.snapshot.val());
-      });
-
-      print("Inside initPolymer: $inboxItems");
-
       print("Polymer ready...");
     });
   });
 
 }
 
+InputElement get _messageInput => querySelector('#messageInput');
 
 doStuff(Event e) {
   e.preventDefault();
   DateTime now = new DateTime.now();
-  var message = querySelector('#messageInput').value;
 
-  List items = querySelectorAll('.item-text');
-  items.forEach((e) {
-    e..text = "New message: $message";
-  });
 
-  print("Here they are:$inboxItems");
+  var message = _messageInput.value;
+
+//  List items = querySelectorAll('.item-text');
+//  items.forEach((e) {
+//    e..text = "New message: $message";
+//  });
+
+  //print("Here they are:$inboxItems");
 
   // Is the following stuff in the right place? It only seems to work properly here.
-  Future setTest(Firebase f) {
+  Future setTest(db.Firebase f) {
     //var setF = f.set({'date': '$now'});
     var pushRef = f.push();
     var setF = pushRef.set('$message');
