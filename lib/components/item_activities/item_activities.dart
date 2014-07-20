3a9a7feddd554b80c25e84171d17826932ac053e
Kai Sellgren <kaisellgren@gmail.com>
2014-07-20 11:53:03 +0300

    Implement comment loading and saving

--- lib/components/item_activities/item_activities.dart
+++ lib/components/item_activities/item_activities.dart
@@ -11,76 +11,59 @@ class ItemActivities extends PolymerElement {
   @published App app;
   @observable List comments = toObservable([]);
 
-  get item { return app; }
+  InputElement get name => $['name'];
+  InputElement get comment => $['comment'];
 
-//  InputElement get name => $['name'];
-//  InputElement get comment => $['comment'];
-
-
-  get formattedDate {
-//    if (app.selectedItem == null) return '';
-//    return InputFormatter.formatMomentDate(app.selectedItem['createdDate'], short: true, momentsAgo: true);
+  String formatItemDate(DateTime value) {
+    return InputFormatter.formatMomentDate(value, short: true, momentsAgo: true);
   }
 
   getActivities() {
-    if (app.selectedItem == null) return;
-    return;
-    print("Got here");
-
     var itemId = app.selectedItem['id'];
     var f = new db.Firebase('https://luminous-fire-4671.firebaseio.com/items/' + itemId + '/activities/comments');
     f.onChildAdded.listen((e) {
       var comment = e.snapshot.val();
       comment['createdDate'] = DateTime.parse(comment['createdDate']);
-
       comment['id'] = e.snapshot.name();
 
-      // Insert each new item at top of list so the list is ascending
+      // Insert each new item at top of list so the list is ascending.
       comments.insert(0, comment);
     });
   }
 
-  // *
+  //
   // Add a comment to /activities/comments for this item.
-  // *
-//  addComment(Event e, var detail, Element target) {
-//    e.preventDefault();
-//
-//    if (name.value.trim().isEmpty) {
-//      window.alert("Your name is empty.");
-//      return false;
-//    }
-//
-//    var itemId = app.selectedItem['id'];
-//
-//    final comments = new db.Firebase('https://luminous-fire-4671.firebaseio.com/items/' + itemId + '/activities/comments');
-//
-//    DateTime now = new DateTime.now().toUtc();
-//
-//    Future set(db.Firebase comments) {
-//      comments.push().set({
-//          'user': name.value,
-//          'comment': comment.value,
-//          'createdDate': '$now'
-//      }).then((e){print('Commented: ' + comment.value);});
-//    }
-//
-//    set(comments);
-//  }
+  //
+  addComment(Event e, var detail, Element target) {
+    e.preventDefault();
+
+    if (name.value.trim().isEmpty) {
+      window.alert("Your name is empty.");
+      return false;
+    }
+
+    var itemId = app.selectedItem['id'];
+
+    final comments = new db.Firebase('https://luminous-fire-4671.firebaseio.com/items/' + itemId + '/activities/comments');
+
+    DateTime now = new DateTime.now().toUtc();
+
+    Future set(db.Firebase comments) {
+      comments.push().set({
+          'user': name.value,
+          'comment': comment.value,
+          'createdDate': '$now'
+      }).then((e){print('Commented: ' + comment.value);});
+    }
+
+    set(comments);
+  }
 
   attached() {
     print("+ItemActivities");
-    //getActivities();
+    getActivities();
 //    app.changes.where((PropertyChangeRecord prop) => prop.name == const Symbol('selectedItem') && prop.newValue != null).first.then(() => getActivities());
   }
 
-  itemChanged() {
-    var selectedItem = app.selectedItem;
-    print("Changed: $selectedItem");
-  }
-
-
-  ItemActivities.created() : super.created() {
-    print(item);
-  }
+  ItemActivities.created() : super.created();
 }
