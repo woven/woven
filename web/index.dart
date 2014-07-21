6e43e39ca0016d2d3a0a364bd494f08ed1974b92
David Notik <dave@woven.org>
2014-07-21 18:19:26 -0400

    Some cleanup, avoided Safari issue for now

--- web/index.dart
+++ web/index.dart
@@ -1,18 +1,19 @@
 import 'package:polymer/polymer.dart';
-import 'package:angular/angular.dart';
-import 'package:angular/application_factory.dart';
-import 'package:angular_node_bind/angular_node_bind.dart';
+//import 'package:angular/angular.dart';
+//import 'package:angular/application_factory.dart';
+//import 'package:angular_node_bind/angular_node_bind.dart';
 import 'dart:html';
 import 'dart:math';
 import 'dart:async';
+export 'package:polymer/init.dart';
 
 void main() {
   // Placeholder for when we want to do stuff after Polymer elements fully loaded
   initPolymer().run(() {
     // Add the node_bind module for Angular
-    applicationFactory()
-      .addModule(new NodeBindModule())
-      .run();
+//    applicationFactory()
+//      .addModule(new NodeBindModule())
+//      .run();
 
     Polymer.onReady.then((_) {
       print("Polymer ready...");
