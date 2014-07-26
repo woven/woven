8d60fa1d2adb0982ca40ee944c93b5627feac7b3
Kai Sellgren <kaisellgren@gmail.com>
2014-07-26 18:15:00 +0300

    Remove the old files

--- lib/components/my_element/my_element.dart
+++
@@ -1,7 +0,0 @@
-import 'package:polymer/polymer.dart';
-
-@CustomTag('my-element')
-class MyElement extends PolymerElement {
-  @published String name = 'Some default';
-  MyElement.created() : super.created();
-}
