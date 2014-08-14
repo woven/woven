d88a23bc8eb5670bd8d5d13e39436de6d56ae3af
David Notik <dave@woven.org>
2014-08-14 09:43:19 -0400

    Missed file

---
+++ lib/src/client/controller/main.dart
@@ -0,0 +1,9 @@
+import '../app.dart';
+import 'dart:html';
+import 'package:woven/src/shared/model/user.dart';
+
+class MainController {
+  static sayWelcome(App app) {
+    window.alert("Welcome!");
+  }
+}
