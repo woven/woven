3a4952bded64ef5253ec4a97966b1a588e1b8e1d
David Notik <dave@woven.co>
2014-09-11 17:39:17 -0400

    Attempt at starred list 2

---
+++ lib/src/client/components/item_list/item_list.dart
@@ -0,0 +1,97 @@
+import 'package:polymer/polymer.dart';
+import 'package:firebase/firebase.dart' as db;
+import 'dart:html';
+import 'package:woven/src/shared/input_formatter.dart';
+import 'package:woven/src/client/app.dart';
+import 'package:core_elements/core_pages.dart';
+import 'package:woven/config/config.dart';
+import 'package:woven/src/client/view_model/main.dart';
+
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
+
+/**
+ * The InboxList class is for the list of inbox items, which is pulled from Firebase.
+ */
+@CustomTag('item-list')
+class ItemList extends PolymerElement with Observable {
+  @published App app;
+  @published MainViewModel viewModel;
+
+  ItemList.created() : super.created();
+
+  InputElement get subject => $['subject'];
+
+  void selectItem(Event e, var detail, Element target) {
+    // Look in the items list for the item that matches the
+    // id passed in the data-id attribute on the element.
+    var item = viewModel.inboxViewModel.items.firstWhere((i) => i['id'] == target.dataset['id']);
+
+    app.selectedItem = item;
+    app.selectedPage = 1;
+    app.userCameFromInbox = true;
+
+    var str = target.dataset['id'];
+    var bytes = UTF8.encode(str);
+    var base64 = CryptoUtils.bytesToBase64(bytes);
+
+    app.router.dispatch(url: "/item/$base64");
+  }
+
+  toggleLike(Event e, var detail, Element target) {
+    e.stopPropagation();
+
+    if (target.classes.contains("selected")) {
+      target.classes.remove("clicked");
+    } else {
+      target.classes.add("clicked");
+    }
+
+    viewModel.inboxViewModel.toggleItemLike(target.dataset['id']);
+  }
+
+  toggleStar(Event e, var detail, Element target) {
+    e.stopPropagation();
+
+    viewModel.inboxViewModel.toggleItemStar(target.dataset['id']);
+  }
+
+  formatItemDate(DateTime value) {
+    return InputFormatter.formatMomentDate(value, short: true, momentsAgo: true);
+  }
+
+
+
+  //Temporary script, about as good a place as any to put it.
+
+//  CreateCommunityItemsScript() {
+//    var f = new db.Firebase(firebaseLocation + '/items');
+//    f.onChildAdded.listen((e) {
+//      var item = e.snapshot.val();
+//      // snapshot.name is Firebase's ID, i.e. "the name of the Firebase location"
+//      // So we'll add that to our local item list.
+//      item['id'] = e.snapshot.name();
+//
+//      final dbRef = new db.Firebase("$firebaseLocation/communities/thelab/item_index/${item['id']}");
+//      set(db.Firebase dbRef) {
+//        dbRef.set({
+//            'itemid': item['id']
+//        });
+//      }
+//
+//      set(dbRef);
+//
+//    });
+//
+//  }
+
+  attached() {
+    print("+Starred");
+    app.pageTitle = "Starred";
+    //CreateCommunityItemsScript();
+  }
+
+  detached() {
+    print("-Starred");
+  }
+}
---
+++ lib/src/client/view_model/list.dart
@@ -0,0 +1,231 @@
+library starred_view_model;
+
+import 'dart:html';
+import 'package:polymer/polymer.dart';
+import 'package:firebase/firebase.dart' as db;
+import 'package:woven/config/config.dart';
+import 'package:woven/src/client/app.dart';
+
+class StarredViewModel extends Observable {
+  final App app;
+  final List items = toObservable([]);
+  final String firebaseLocation = config['datastore']['firebaseLocation'];
+
+  StarredViewModel(this.app) {
+//    switch (list) {
+//      case "starred":
+        loadStarredItemsForUser();
+//        break;
+//      default:
+//        loadStarredItemsForUser();
+//    }
+  }
+
+  /**
+   * Loads the user's starred items.
+   */
+  void loadStarredItemsForUser() {
+    var f = new db.Firebase(firebaseLocation);
+    var starredItemsByUserRef = f.child('/starred_by_user/' + app.user.username + '/items');
+
+    // TODO: Undo the limit of 20; https://github.com/firebase/firebase-dart/issues/8
+    starredItemsByUserRef.limit(20).onChildAdded.listen((e) {
+      bool itemExists = false;
+      if (items.isEmpty) {
+        itemExists = false;
+      } else {
+        itemExists = (items.firstWhere((i) => i['id'] == e.snapshot.name()) != null) ? true : false;
+        if (itemExists) return;
+      }
+      print("First step.");
+
+      f.child('/items/' + e.snapshot.name()).onValue.listen((e) {
+
+        bool itemExists = false;
+        if (items.isEmpty) {
+          itemExists = false;
+        } else {
+          itemExists = (items.firstWhere((i) => i['id'] == e.snapshot.name()) != null) ? true : false;
+          if (itemExists) return;
+        }
+
+
+        print("Got here...");
+        print(itemExists);
+
+        var item = toObservable(e.snapshot.val());
+
+        // If no updated date, use the created date.
+        if (item['updatedDate'] == null) {
+          item['updatedDate'] = item['createdDate'];
+        }
+
+        // The live-date-time element needs parsed dates.
+        item['updatedDate'] = DateTime.parse(item['updatedDate']);
+        item['createdDate'] = DateTime.parse(item['createdDate']);
+
+        // snapshot.name is Firebase's ID, i.e. "the name of the Firebase location"
+        // So we'll add that to our local item list.
+        item['id'] = e.snapshot.name();
+
+        // Insert each new item into the list.
+        items.add(toObservable(item));
+
+        // Sort the list by the item's updatedDate.
+        items.sort((m1, m2) => m2["updatedDate"].compareTo(m1["updatedDate"]));
+
+        // Listen for realtime changes to the star count.
+        f.child('/items/' + item['id'] + '/star_count').onValue.listen((e) {
+          item['star_count'] = (e.snapshot.val() != null) ? e.snapshot.val() : 0;
+        });
+
+        // Listen for realtime changes to the like count.
+        f.child('/items/' + item['id'] + '/like_count').onValue.listen((e) {
+          item['like_count'] = (e.snapshot.val() != null) ? e.snapshot.val() : 0;
+        });
+
+        if (app.user != null) {
+          var starredItemsRef = new db.Firebase(firebaseLocation + '/starred_by_user/' + app.user.username + '/items/' + item['id']);
+          var likedItemsRef = new db.Firebase(firebaseLocation + '/liked_by_user/' + app.user.username + '/items/' + item['id']);
+          starredItemsRef.onValue.listen((e) {
+            item['starred'] = e.snapshot.val() != null;
+          });
+          likedItemsRef.onValue.listen((e) {
+            item['liked'] = e.snapshot.val() != null;
+          });
+        } else {
+          item['starred'] = false;
+          item['liked'] = false;
+        }
+      });
+    });
+
+//    loadUserStarredItemInformation();
+//    loadUserLikedItemInformation();
+
+
+  }
+
+  void toggleItemStar(id) {
+    if (app.user == null) return app.showMessage("Kindly sign in first.", "important");
+
+    var item = items.firstWhere((i) => i['id'] == id);
+
+    var firebaseRoot = new db.Firebase(firebaseLocation);
+    var starredItemRef = firebaseRoot.child('/starred_by_user/' + app.user.username + '/items/' + item['id']);
+    var itemRef = firebaseRoot.child('/items/' + item['id']);
+
+    if (item['starred']) {
+      // If it's starred, time to unstar it.
+      item['starred'] = false;
+      starredItemRef.remove();
+
+      // Update the star count.
+      itemRef.child('/star_count').transaction((currentCount) {
+        if (currentCount == null || currentCount == 0) {
+          item['star_count'] = 0;
+          return 0;
+        } else {
+          item['star_count'] = currentCount - 1;
+          return item['star_count'];
+        }
+      });
+
+      // Update the list of users who starred.
+      firebaseRoot.child('/users_who_starred/item/' + item['id'] + '/' + app.user.username).remove();
+    } else {
+      // If it's not starred, time to star it.
+      item['starred'] = true;
+      starredItemRef.set(true);
+
+      // Update the star count.
+      itemRef.child('/star_count').transaction((currentCount) {
+        if (currentCount == null || currentCount == 0) {
+          item['star_count'] = 1;
+          return 1;
+        } else {
+          item['star_count'] = currentCount + 1;
+          return item['star_count'];
+        }
+      });
+
+      // Update the list of users who starred.
+      firebaseRoot.child('/users_who_starred/item/' + item['id'] + '/' + app.user.username).set(true);
+    }
+  }
+
+  void toggleItemLike(id) {
+    if (app.user == null) return app.showMessage("Kindly sign in first.", "important");
+
+    var item = items.firstWhere((i) => i['id'] == id);
+
+    var firebaseRoot = new db.Firebase(firebaseLocation);
+    var starredItemRef = firebaseRoot.child('/liked_by_user/' + app.user.username + '/items/' + item['id']);
+    var itemRef = firebaseRoot.child('/items/' + item['id']);
+
+    if (item['liked']) {
+      // If it's starred, time to unstar it.
+      item['liked'] = false;
+      starredItemRef.remove();
+
+      // Update the star count.
+      itemRef.child('/like_count').transaction((currentCount) {
+        if (currentCount == null || currentCount == 0) {
+          item['like_count'] = 0;
+          return 0;
+        } else {
+          item['like_count'] = currentCount - 1;
+          return item['like_count'];
+        }
+      });
+
+      // Update the list of users who liked.
+      firebaseRoot.child('/users_who_liked/item/' + item['id'] + '/' + app.user.username).remove();
+    } else {
+      // If it's not starred, time to star it.
+      item['liked'] = true;
+      starredItemRef.set(true);
+
+      // Update the star count.
+      itemRef.child('/like_count').transaction((currentCount) {
+        if (currentCount == null || currentCount == 0) {
+          item['like_count'] = 1;
+          return 1;
+        } else {
+          item['like_count'] = currentCount + 1;
+          return item['like_count'];
+        }
+      });
+
+      // Update the list of users who liked.
+      firebaseRoot.child('/users_who_liked/item/' + item['id'] + '/' + app.user.username).set(true);
+    }
+  }
+
+  void loadUserStarredItemInformation() {
+    items.forEach((item) {
+      if (app.user != null) {
+        var starredItemsRef = new db.Firebase(firebaseLocation + '/starred_by_user/' + app.user.username + '/items/' + item['id']);
+        starredItemsRef.onValue.listen((e) {
+          item['starred'] = e.snapshot.val() != null;
+        });
+      } else {
+        item['starred'] = false;
+      }
+
+    });
+  }
+
+  void loadUserLikedItemInformation() {
+    items.forEach((item) {
+      if (app.user != null) {
+        var starredItemsRef = new db.Firebase(firebaseLocation + '/liked_by_user/' + app.user.username + '/items/' + item['id']);
+        starredItemsRef.onValue.listen((e) {
+          item['liked'] = e.snapshot.val() != null;
+        });
+      } else {
+        item['liked'] = false;
+      }
+    });
+  }
+}
