b12d0cc6d97a67c30a36e970aa8910d4c2eef5ef
David Notik <dave@woven.co>
2014-10-03 15:53:21 -0400

    Introduce learn more CTA on community list (part is in community_list.html commit too)

---
+++ lib/src/client/components/cta_learn_more/cta_learn_more.dart
@@ -0,0 +1,60 @@
+import 'package:polymer/polymer.dart';
+import 'dart:html';
+import 'dart:async' show Timer;
+import 'dart:math' show Random;
+import 'package:woven/src/client/app.dart';
+
+@CustomTag('cta-learn-more')
+class LearnMoreCta extends PolymerElement {
+  LearnMoreCta.created() : super.created();
+
+  @published App app;
+  @published bool opened = false;
+  @observable String randomWord = "";
+
+  /**
+   * Toggle the page.
+   */
+  togglePage() {
+  }
+
+  void handleCallToAction() {
+    // Reset the current selectedItem so item-view grabs it from the URL
+    app.selectedItem == null;
+    app.router.dispatch(url: "item/LUpWdzZXaWd4dHdvRWM1ZGNhdXo=");
+  }
+
+  animateRandomWords() {
+    List words = ['changemakers', 'community organizers', 'lovers', 'doers', 'movers and shakers',
+    'collaborators', 'the crazy ones', 'makers', 'builders', 'world changers', 'we vs. me', 'repairing the world',
+    'collective action', 'collaborative networking'];
+    words.shuffle(new Random());
+
+    randomWord = words[0]; // Initial random word.
+    var i = 1; // Because we already used the first word, start iterating at the second.
+
+    Timer timer = new Timer.periodic(new Duration(seconds: 4), (f) {
+      HtmlElement el;
+      el = $['random-word'];
+      el.style.opacity = '0';
+
+      new Timer(new Duration(milliseconds: 750), () {
+        el.style.opacity = '1';
+        if (i == words.length) {
+          i = 0;
+        }
+        randomWord = words[i];
+        i++;
+      });
+    });
+  }
+
+  signIn(Event e) {
+    e.stopPropagation();
+    app.signInWithFacebook();
+  }
+
+  attached() {
+    animateRandomWords();
+  }
+}
