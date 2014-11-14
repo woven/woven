6ba8b013a8800e7f9ea8b932ba7a476461459d21
David Notik <dave@woven.co>
2014-11-13 20:35:03 -0500

    Debug

--- lib/src/server/controller/main.dart
+++ lib/src/server/controller/main.dart
@@ -142,7 +142,7 @@ http://twitter.com/wovenco
 
     Future findCommentInfo() {
       return Firebase.get('/items/$item/activities/comments/$comment.json').then((commentData) {
-        print("Comment: $comment // $commentData");
+        print("Item $item // Comment: $comment // $commentData");
         notificationData['commentText'] = commentData['comment'];
         notificationData['commentAuthor'] = commentData['user'];
       });
