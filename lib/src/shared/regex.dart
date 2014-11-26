0710166cda13f0efa78a0c9e55ec2ac0b22a56bd
David Notik <dave@woven.co>
2014-11-25 20:46:35 -0500

    Improve regex

--- lib/src/shared/regex.dart
+++ lib/src/shared/regex.dart
@@ -1,5 +1,5 @@
 class RegexHelper {
-  static String domain = '[a-zA-Z.]+';
+  static String domain = '[a-zA-Z0-9.-]+[a-zA-Z0-9-]';
   static String protocol = '[a-zA-Z]+:\\/\\/';
   static String www = 'www\\.';
 
@@ -7,10 +7,10 @@ class RegexHelper {
   static String email = '\\b${emailName}@${domain}\\b';
 
   static String queryPath = '\\/[-~+=%_a-zA-Z0-9.]*[-~+=%_a-zA-Z0-9]';
-  static String searchString = '\\?[-+=&;%@_.a-zA-Z0-9]+[-+=&;%@_a-zA-Z0-9]';
+  static String searchString = '\\?[-+=&;%@_.a-zA-Z0-9]*[-+=&;%@_a-zA-Z0-9]';
   static String queryHash = '#[-=_a-zA-Z0-9]+';
 
-  static String link = '\\b($protocol|$protocol$www|$www)$domain($queryPath)*(\\/?$searchString)?($queryHash)?';
+  static String link = '\\b($protocol|$protocol$www|$www)$domain($queryPath)*\\/?(\\/?$searchString)?($queryHash)?';
 
   static String linkOrEmail = '($email|$link)';
 }
