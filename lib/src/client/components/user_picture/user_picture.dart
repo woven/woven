b5b55911f21827e9d26e995aa1fb39c1454f65b5
David Notik <dave@woven.co>
2015-01-05 17:21:39 -0500

    Rough go at user pictures in inbox

--- lib/src/client/components/user_picture/user_picture.dart
+++ lib/src/client/components/user_picture/user_picture.dart
@@ -2,14 +2,30 @@ library user_picture;
 
 import "dart:html";
 
-import "package:polymer/polymer.dart";
+import 'package:polymer/polymer.dart';
+import 'package:firebase/firebase.dart';
 
-import "package:woven/src/client/app.dart";
+import 'package:woven/src/client/app.dart';
+import 'package:woven/config/config.dart';
 
 @CustomTag("user-picture")
 class UserPicture extends PolymerElement {
   @published App app;
+  @published String user;
+  @observable Map userMap;
+
+  var fb = new Firebase(config['datastore']['firebaseLocation']);
 
   UserPicture.created() : super.created();
 
+  attached() {
+    if (user != null) {
+      fb.child('/users/$user').once('value').then((res) {
+        userMap = res.val();
+        userMap['fullPicturePath'] = '${config['google']['cloudStoragePath']}/${userMap['picture']}';
+//        print(userMap);
+      });
+
+    }
+  }
 }
