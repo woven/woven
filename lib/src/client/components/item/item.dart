ead92413f27fb182c46723133865e0c66af84515
David Notik <dave@woven.co>
2015-01-13 16:45:36 -0500

    New x-item element, style improvements

---
+++ lib/src/client/components/item/item.dart
@@ -0,0 +1,81 @@
+import 'package:polymer/polymer.dart';
+import 'package:firebase/firebase.dart' as db;
+import 'dart:html';
+import 'dart:async';
+import 'package:woven/src/shared/input_formatter.dart';
+import 'package:woven/src/client/app.dart';
+import 'package:woven/config/config.dart';
+import 'package:woven/src/client/view_model/feed.dart';
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
+import 'package:woven/src/client/infinite_scroll.dart';
+import 'package:core_elements/core_header_panel.dart';
+
+/**
+ * A list of items.
+ */
+@CustomTag('x-item')
+class Item extends PolymerElement with Observable {
+  @published Map item;
+  @published App app;
+  @published FeedViewModel viewModel;
+
+  List<StreamSubscription> subscriptions = [];
+
+  Item.created() : super.created();
+
+  InputElement get subject => $['subject'];
+
+  void selectItem(Event e, var detail, Element target) {
+    // Look in the items list for the item that matches the
+    // id passed in the data-id attribute on the element.
+    var item = viewModel.items.firstWhere((i) => i['id'] == target.dataset['id']);
+
+    app.previousPage = app.selectedPage;
+    app.selectedItem = item;
+    app.selectedPage = 1;
+
+
+    var str = target.dataset['id'];
+    var bytes = UTF8.encode(str);
+    var base64 = CryptoUtils.bytesToBase64(bytes);
+
+    app.router.dispatch(url: "/item/$base64");
+  }
+
+  toggleLike(Event e, var detail, Element target) {
+    e.stopPropagation();
+
+//    if (target.classes.contains("selected")) {
+//      target.classes.remove("clicked");
+//    } else {
+//      target.classes.add("clicked");
+//    }
+
+    viewModel.toggleItemLike(target.dataset['id']);
+  }
+
+  toggleStar(Event e, var detail, Element target) {
+    e.stopPropagation();
+
+    viewModel.toggleItemStar(target.dataset['id']);
+  }
+
+  formatItemDate(DateTime value) {
+    return InputFormatter.formatMomentDate(value, short: true, momentsAgo: true);
+  }
+
+  formatEventDate(DateTime startDate) {
+    // TODO: Bring back endDate, currently null.
+    return InputFormatter.formatDate(startDate.toLocal(), showHappenedPrefix: true, trimPast: true);
+  }
+
+
+  attached() {
+    //
+  }
+
+  detached() {
+    //
+  }
+}
