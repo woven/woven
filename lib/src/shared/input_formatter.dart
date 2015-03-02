475df3d94a27424ffa5a9dea6de8c5c59b893047
David Notik <dave@woven.co>
2015-03-02 16:50:55 -0500

    Improve regex for @mentions and links

--- lib/src/shared/input_formatter.dart
+++ lib/src/shared/input_formatter.dart
@@ -581,7 +581,7 @@ class InputFormatter {
   static String formatMentions(String content) {
     if (content == null) return '';
 
-    var regExp = new RegExp(r'\B@[a-zA-Z0-9_-]+', caseSensitive: false);
+    var regExp = new RegExp(r'(^|\s+)@[a-zA-Z0-9_-]+(\s+|$)', caseSensitive: false);
 
     content = content.replaceAllMapped(regExp, (Match m) => '<span class="mention">${m[0]}</span>');
 
