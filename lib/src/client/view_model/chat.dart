08b8cf87360f4b34bbfc9fcd4222a13dc152baca
David Notik <dave@woven.co>
2015-03-09 16:03:30 -0400

    Update local message with server side timestamps

--- lib/src/client/view_model/chat.dart
+++ lib/src/client/view_model/chat.dart
@@ -136,6 +136,8 @@ class ChatViewModel extends BaseViewModel with Observable {
       if (existingItem != null) {
         // Pass the ID to the existing item as we might not have it.
         existingItem['id'] = newItem['id'];
+        existingItem['createdDate'] = newItem['createdData'];
+        existingItem['updatedDate'] = newItem['updatedData'];
       } else {
         // Insert each new item into the list.
         insertMessage(newItem);
