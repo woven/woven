f165969a2b0358a06903695d69c9462b0f50d962
David Notik <dave@woven.co>
2015-03-30 12:32:45 -0400

    Decrease chat page size

--- lib/src/client/view_model/chat.dart
+++ lib/src/client/view_model/chat.dart
@@ -20,7 +20,7 @@ class ChatViewModel extends BaseViewModel with Observable {
   // TODO: Use this later for date separators between messages.
   final Map groupedItems = toObservable({});
 
-  int pageSize = 50;
+  int pageSize = 25;
   @observable bool reloadingContent = false;
   @observable bool reachedEnd = false;
   @observable bool isScrollPosAtBottom = false;
