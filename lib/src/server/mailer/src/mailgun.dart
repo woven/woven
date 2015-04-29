843ad9c342794a65bcd7ddef0deb9fd16a8e90b1
David Notik <dave@woven.co>
2015-04-29 17:25:42 -0400

    Modify MailGun for async/await, move method to Envelope

--- lib/src/server/mailer/src/mailgun.dart
+++ lib/src/server/mailer/src/mailgun.dart
@@ -5,44 +5,29 @@ class Mailgun {
   /**
    * Send a message.
    */
-  Future send(Envelope envelope) {
+  static Future send(Envelope envelope) async {
     var user = config['mailgun']['user'];
     var key = config['mailgun']['key'];
     var path = config['mailgun']['path'];
 
     final auth = CryptoUtils.bytesToBase64(UTF8.encode("$user:$key"));
     final url = "${config['mailgun']['path']}/messages";
-    return http.post(url, body: prepareEnvelopeForPost(envelope), headers: {"authorization": "Basic $auth"})
-    .then((response) {
-      Map responseAsMap = {};
-      try {
-        responseAsMap = JSON.decode(response.body);
-      } catch(error) {
-        responseAsMap = {'error': '$error'};
-        print("Error in MailGun response:\n$error\nMailGun response was:\n${response.body}");
-      }
 
-      return {'status': response.statusCode, 'response': responseAsMap};
-//      if (response.statusCode == 400) {
-//        throw 'Mailgun returned an error.\nPath: $url\nData: ${envelope.toMap()}\nResponse: ${responseAsMap["message"]}';
-//      }
-//      if (response.statusCode == 200) {
-//        return responseAsMap['message'];
-//      }
-    });
-  }
+    http.Response response = await http.post(
+        url,
+        body: Envelope.prepareForPost(envelope),
+        headers: {"authorization": "Basic $auth"}
+    );
 
-  /**
-   * Prepare the envelope for transmission over http POST.
-   *
-   * Removes null properties and converts lists to strings.
-   */
-  prepareEnvelopeForPost(Envelope e) {
-    Map map = e.toMap();
-    Map newMap = new Map.from(map);
-    map.forEach((k, v) {
-      if (v is List) newMap[k] = (v as List).join(',');
-    });
-    return removeNullsFromMap(newMap);
+    Map responseAsMap = {};
+
+    try {
+      responseAsMap = JSON.decode(response.body);
+    } catch (error) {
+      responseAsMap = {'error': '$error'};
+      print("Error in MailGun response:\n$error\nMailGun response was:\n${response.body}");
+    }
+
+    return {'status': response.statusCode, 'response': responseAsMap};
   }
 }
