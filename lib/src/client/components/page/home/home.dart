52198c04ce9fa0cd4517d28527d3ff8078f2c5e3
David Notik <dave@woven.co>
2015-10-12 14:01:05 -0400

    Start new homepage

--- lib/src/client/components/page/home/home.dart
+++ lib/src/client/components/page/home/home.dart
@@ -533,31 +533,6 @@ class Home extends PolymerElement with Observable {
     }
   }
 
-  animateRandomWords() {
-    List words = ['changemakers', 'community organizers', 'movers and shakers',
-    'collaborators', 'the crazy ones', 'makers', 'world changers', 'we vs. me', 'repairing the world',
-    'collective action', 'collaborative networking'];
-    words.shuffle(new Random());
-
-    randomWord = words[0]; // Initial random word.
-    var i = 1; // Because we already used the first word, start iterating at the second.
-
-    Timer timer = new Timer.periodic(new Duration(seconds: 4), (f) {
-      HtmlElement el;
-      el = $['random-word'];
-      el.style.opacity = '0';
-
-      new Timer(new Duration(milliseconds: 750), () {
-        el.style.opacity = '1';
-        if (i == words.length) {
-          i = 0;
-        }
-        randomWord = words[i];
-        i++;
-      });
-    });
-  }
-
   attached() {
     if (app.debugMode) print('+Home');
 
