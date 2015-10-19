bafa0a96925fb25a8e683069b1983ea83f9ad93c
David Notik <dave@woven.co>
2015-10-19 12:37:53 -0400

    Fix up inline-item, listens for uriPreview changes

--- lib/src/client/components/inline_item/inline_item.dart
+++ lib/src/client/components/inline_item/inline_item.dart
@@ -22,7 +22,9 @@ class InlineItem extends PolymerElement with Observable {
 
   db.Firebase get f => app.f;
 
-//  List<StreamSubscription> subscriptions = [];
+  get itemRef => f.child('/items/' + itemId);
+
+  List<StreamSubscription> subscriptions = [];
 
   InlineItem.created() : super.created();
 
@@ -53,105 +55,142 @@ class InlineItem extends PolymerElement with Observable {
   /**
    * Get the item.
    */
-  void getItem() {
-//    if (item.length == 0) onLoadCompleter.complete(true);
-
-    // If there's no app.selectedItem, we probably
-    // came here directly, so let's get it using the URL.
-//    var encodedItem = Uri.parse(window.location.toString()).pathSegments[1];
-//    var decodedItem = base64Decode(encodedItem);
-
-    f.child('/items/' + itemId).onValue.first.then((e) {
-      // We hold the item in a separate var while we pre-process it.
+  getItem() {
+    StreamSubscription subscription = itemRef.onValue.listen((e) {
       var queuedItem = toObservable(e.snapshot.val());
 
       // Make sure we're using the collapsed username.
       queuedItem['user'] = (queuedItem['user'] as String).toLowerCase();
 
-      return UserModel.usernameForDisplay(queuedItem['user'], f, app.cache).then((String usernameForDisplay) {
-        queuedItem['usernameForDisplay'] = usernameForDisplay;
+//      String usernameForDisplay = await UserModel.usernameForDisplay(queuedItem['user'], f, app.cache);
+//      queuedItem['usernameForDisplay'] = usernameForDisplay;
 
-        // If no updated date, use the created date.
-        // TODO: We assume createdDate is never null!
-        if (queuedItem['updatedDate'] == null) {
-          queuedItem['updatedDate'] = queuedItem['createdDate'];
-        }
+      // If no updated date, use the created date.
+      // TODO: We assume createdDate is never null!
+      if (queuedItem['updatedDate'] == null) {
+        queuedItem['updatedDate'] = queuedItem['createdDate'];
+      }
 
-        // The live-date-time element needs parsed dates.
-        queuedItem['createdDate'] = DateTime.parse(queuedItem['createdDate']);
-        queuedItem['updatedDate'] = DateTime.parse(queuedItem['updatedDate']);
+      // The live-date-time element needs parsed dates.
+      queuedItem['createdDate'] = DateTime.parse(queuedItem['createdDate']);
+      queuedItem['updatedDate'] = DateTime.parse(queuedItem['updatedDate']);
 
-        switch (queuedItem['type']) {
-          case 'event':
-            if (queuedItem['startDateTime'] != null) queuedItem['startDateTime'] = DateTime.parse(queuedItem['startDateTime']);
-            queuedItem['defaultImage'] = 'event';
-            break;
-          case 'announcement':
-            queuedItem['defaultImage'] = 'announcement';
-            break;
-          case 'news':
-            queuedItem['defaultImage'] = 'custom-icons:news';
-            break;
-          case 'message':
-          case 'other':
-            break;
-          default:
-        }
+      switch (queuedItem['type']) {
+        case 'event':
+          if (queuedItem['startDateTime'] != null) queuedItem['startDateTime'] = DateTime.parse(queuedItem['startDateTime']);
+          queuedItem['defaultImage'] = 'event';
+          break;
+        case 'announcement':
+          queuedItem['defaultImage'] = 'announcement';
+          break;
+        case 'news':
+          queuedItem['defaultImage'] = 'custom-icons:news';
+          break;
+        case 'message':
+        case 'other':
+          break;
+        default:
+      }
 
-        // Handle any URI previews the item may have.
-        if (queuedItem['uriPreviewId'] != null) {
-          f.child('/uri_previews/${queuedItem['uriPreviewId']}').onValue.listen((e) {
-            var previewData = e.snapshot.val();
-            UriPreview preview = UriPreview.fromJson(previewData);
-            queuedItem['uriPreview'] = preview.toJson();
-            queuedItem['uriPreview']['imageSmallLocation'] = (queuedItem['uriPreview']['imageSmallLocation'] != null) ? '${app.cloudStoragePath}/${queuedItem['uriPreview']['imageSmallLocation']}' : null;
-            queuedItem['uriPreviewTried'] = true;
-
-            // If subject and body are empty, use title and teaser from URI preview instead.
-            if (queuedItem['subject'] == null) queuedItem['subject'] = toObservable(preview.title);
-            if (queuedItem['body'] == null) queuedItem['body'] =  toObservable(preview.teaser);;
-          });
-        } else {
+      // Handle any URI previews the item may have.
+      if (queuedItem['uriPreviewId'] != null) {
+        f.child('/uri_previews/${queuedItem['uriPreviewId']}').onValue.listen((e) {
+          var previewData = e.snapshot.val();
+          UriPreview preview = UriPreview.fromJson(previewData);
+          queuedItem['uriPreview'] = preview.toJson();
+          queuedItem['uriPreview']['imageSmallLocation'] = (queuedItem['uriPreview']['imageSmallLocation'] != null) ? '${app.cloudStoragePath}/${queuedItem['uriPreview']['imageSmallLocation']}' : null;
           queuedItem['uriPreviewTried'] = true;
-        }
 
-        // Format the URL for display.
-        if (queuedItem['url'] != null) {
-          String uriHost = Uri.parse(queuedItem['url']).host;
-          String uriHostShortened = uriHost.substring(uriHost.toString().lastIndexOf(".", uriHost.toString().lastIndexOf(".") - 1) + 1);
-          queuedItem['uriHost'] = uriHostShortened;
-        }
-
-        // snapshot.key is Firebase's ID, i.e. "the name of the Firebase location"
-        // So we'll add that to our local item.
-        queuedItem['id'] = e.snapshot.key;
-
-        queuedItem['formattedBody'] = InputFormatter.createTeaser(queuedItem['body'], 75);
-
-        // Listen for realtime changes to the star count.
-        f.child('/items/' + queuedItem['id'] + '/star_count').onValue.listen((e) {
-          queuedItem['star_count'] = (e.snapshot.val() != null) ? e.snapshot.val() : 0;
+          // If subject and body are empty, use title and teaser from URI preview instead.
+          if (queuedItem['subject'] == null) queuedItem['subject'] = toObservable(preview.title);
+          if (queuedItem['body'] == null) queuedItem['body'] = toObservable(preview.teaser);;
         });
+      } else {
+        queuedItem['uriPreviewTried'] = true;
+      }
 
-        // Listen for realtime changes to the like count.
-        f.child('/items/' + queuedItem['id'] + '/like_count').onValue.listen((e) {
-          queuedItem['like_count'] = (e.snapshot.val() != null) ? e.snapshot.val() : 0;
-        });
+      // Format the URL for display.
+      if (queuedItem['url'] != null) {
+        String uriHost = Uri.parse(queuedItem['url']).host;
+        String uriHostShortened = uriHost.substring(uriHost.toString().lastIndexOf(".", uriHost.toString().lastIndexOf(".") - 1) + 1);
+        queuedItem['uriHost'] = uriHostShortened;
+      }
 
-        // Listen for realtime changes to the comment count.
-        f.child('/items/' + queuedItem['id'] + '/comment_count').onValue.listen((e) {
-          queuedItem['comment_count'] = (e.snapshot.val() != null) ? e.snapshot.val() : 0;
-        });
+      // snapshot.key is Firebase's ID, i.e. "the name of the Firebase location"
+      // So we'll add that to our local item.
+      queuedItem['id'] = e.snapshot.key;
 
-        item = queuedItem;
+      queuedItem['formattedBody'] = InputFormatter.createTeaser(queuedItem['body'], 75);
+
+      // Listen for realtime changes to the star count.
+      f.child('/items/' + queuedItem['id'] + '/star_count').onValue.listen((e) {
+        queuedItem['star_count'] = (e.snapshot.val() != null) ? e.snapshot.val() : 0;
       });
-    }).then((e) {
+
+      // Listen for realtime changes to the like count.
+      f.child('/items/' + queuedItem['id'] + '/like_count').onValue.listen((e) {
+        queuedItem['like_count'] = (e.snapshot.val() != null) ? e.snapshot.val() : 0;
+      });
+
+      // Listen for realtime changes to the comment count.
+      f.child('/items/' + queuedItem['id'] + '/comment_count').onValue.listen((e) {
+        queuedItem['comment_count'] = (e.snapshot.val() != null) ? e.snapshot.val() : 0;
+      });
+
+      item = queuedItem;
+
       loadItemUserStarredLikedInformation();
     });
+
+    subscriptions.add(subscription);
+
+//    listenForChanges();
+  }
+
+  /* Deprecated at the moment. */
+  listenForChanges()  {
+    // Listen for changed items.
+
+    var onChildChanged = itemRef.onChildChanged.listen((e) {
+      Map currentData = item;
+      Map newData = e.snapshot.val();
+
+      // Make sure we're using the collapsed username.
+      newData['user'] = (newData['user'] as String).toLowerCase();
+
+      Future processData = new Future.sync(() {
+        // First pre-process some things.
+        if (newData['createdDate'] != null) newData['createdDate'] = DateTime.parse(newData['createdDate']);
+        if (newData['updatedDate'] != null) newData['updatedDate'] = DateTime.parse(newData['updatedDate']);
+        if (newData['startDateTime'] != null) newData['startDateTime'] = DateTime.parse(newData['startDateTime']);
+        if (newData['star_count'] == null) newData['star_count'] = 0;
+        if (newData['like_count'] == null) newData['like_count'] = 0;
+
+        if (newData['uriPreviewId'] != null) {
+          // Get the associated URI preview.
+          return f.child('/uri_previews/${newData['uriPreviewId']}').once('value').then((e) {
+            var previewData = e.val();
+            UriPreview preview = UriPreview.fromJson(previewData);
+            newData['uriPreview'] = preview.toJson();
+            newData['uriPreview']['imageSmallLocation'] = (newData['uriPreview']['imageSmallLocation'] != null) ? '${app.cloudStoragePath}/${newData['uriPreview']['imageSmallLocation']}' : null;
+            newData['uriPreviewTried'] = true;
+
+            // If item's subject/body are empty, use any title/teaser from URI preview instead.
+            if (newData['subject'] == null) newData['subject'] = preview.title;
+            if (newData['body'] == null) newData['body'] = preview.teaser;
+          });
+        }
+      }).then((_) {
+        // Now that new data is pre-processed, update current data.
+        newData.forEach((k, v) => currentData[k] = v);
+        item = currentData;
+      });
+    });
+    subscriptions.add(onChildChanged);
   }
 
   void loadItemUserStarredLikedInformation() {
-    if (item['id'] == null) return;
+    if (itemId == null) return;
 
     if (app.user != null && !item.isEmpty) {
       var starredItemsRef = f.child('/starred_by_user/' + app.user.username.toLowerCase() + '/items/' + item['id']);
@@ -261,30 +300,16 @@ class InlineItem extends PolymerElement with Observable {
   /**
    * Format the given string with "a" or "an" or none.
    */
-  formatWordArticle(String content) {
-    return InputFormatter.formatWordArticle(content);
-  }
+  formatWordArticle(String content) => InputFormatter.formatWordArticle(content);
 
+  formatItemDate(DateTime value) => InputFormatter.formatMomentDate(value, short: true, momentsAgo: true);
 
-  formatItemDate(DateTime value) {
-    return InputFormatter.formatMomentDate(value, short: true, momentsAgo: true);
-  }
+  // TODO: Bring back endDate, currently null.
+  formatEventDate(DateTime startDate) => InputFormatter.formatDate(startDate.toLocal(), showHappenedPrefix: true, trimPast: true);
 
-  formatEventDate(DateTime startDate) {
-    // TODO: Bring back endDate, currently null.
-    return InputFormatter.formatDate(startDate.toLocal(), showHappenedPrefix: true, trimPast: true);
-  }
+  stopProp(Event e) => e.stopPropagation();
 
-  stopProp(Event e) {
-    e.stopPropagation();
-  }
+  attached() => getItem();
 
-
-  attached() {
-    getItem();
-  }
-
-  detached() {
-    //
-  }
+  detached() => subscriptions.forEach((subscription) => subscription.cancel());
 }
