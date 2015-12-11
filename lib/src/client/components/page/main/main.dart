572be5ba04fb37da2af0a5cd24ad4e47050c49f7
David Notik <dave@woven.co>
2015-12-11 00:03:20 -0500

    Line height, disable tooltips

--- lib/src/client/components/page/main/main.dart
+++ lib/src/client/components/page/main/main.dart
@@ -6,9 +6,9 @@ import 'dart:convert';
 
 import 'package:polymer/polymer.dart';
 //import 'package:core_elements/core_drawer_panel.dart';
-import 'package:core_elements/core_tooltip.dart';
+//import 'package:core_elements/core_tooltip.dart';
 import 'package:core_elements/core_image.dart';
-import 'package:core_elements/core_dropdown.dart';
+//import 'package:core_elements/core_dropdown.dart';
 
 import 'package:woven/src/client/app.dart';
 import 'package:woven/src/shared/routing/routes.dart';
