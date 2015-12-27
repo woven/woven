c2f9e26802b7ac243929b9b6ed406b59a42cb5b0
David Notik <dave@woven.co>
2015-12-26 22:44:13 -0500

    Import

--- lib/src/client/components/page/woven_app/woven_app.dart
+++ lib/src/client/components/page/woven_app/woven_app.dart
@@ -1,8 +1,10 @@
+@HtmlImport('woven_app.html')
 library woven_app;
 
 import 'package:polymer/polymer.dart';
 
 import 'package:woven/src/client/app.dart';
+import 'package:woven/src/client/components/page/main/main.dart';
 
 @CustomTag('woven-app')
 class WovenApp extends PolymerElement with Observable {
