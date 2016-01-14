a8b32f62fbba0325844814d863e333f1475aff71
David Notik <dave@woven.co>
2016-01-13 19:26:27 -0500

    Print error

--- lib/src/server/daemon/daemon.dart
+++ lib/src/server/daemon/daemon.dart
@@ -22,6 +22,7 @@ class Daemon {
     Logger.root.level = Level.ALL;
     Logger.root.onRecord.listen((LogRecord rec) {
       print('${rec.level.name}: ${rec.time}: ${rec.message}');
+      if (rec.error != null) print(rec.error);
       if (rec.stackTrace != null) print(rec.stackTrace);
     });
 
