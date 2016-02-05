39d1a6cf8e356c20c98b01929e1fe1d3b528a5fb
David Notik <dave@woven.co>
2016-02-05 00:49:15 -0500

    dartfmt

--- lib/src/server/task/crawler.dart
+++ lib/src/server/task/crawler.dart
@@ -7,13 +7,6 @@ import 'package:html/parser.dart' show parse;
 import 'package:html/dom.dart';
 import 'package:http/http.dart' as http;
 import 'package:firebase/firebase_io.dart';
-
-import 'package:woven/src/server/daemon/daemon.dart';
-import 'package:woven/src/shared/model/uri_preview.dart';
-import 'package:woven/src/shared/model/feed.dart';
-import 'package:woven/src/shared/model/news.dart';
-import 'package:woven/src/server/model/item.dart';
-import 'package:woven/src/shared/util.dart' as util;
 import 'package:logging/logging.dart';
 
 import 'task.dart';
@@ -28,6 +21,12 @@ import 'package:woven/src/server/crawler/feed_reader.dart';
 import 'package:woven/src/server/model/feed_item.dart';
 import 'package:woven/src/server/crawler/open_graph.dart';
 import 'package:woven/src/server/crawler/image_info.dart';
+import 'package:woven/src/server/daemon/daemon.dart';
+import 'package:woven/src/shared/model/uri_preview.dart';
+import 'package:woven/src/shared/model/feed.dart';
+import 'package:woven/src/shared/model/news.dart';
+import 'package:woven/src/server/model/item.dart';
+import 'package:woven/src/shared/util.dart' as util;
 
 class CrawlerTask extends Task {
   Daemon app;
@@ -51,8 +50,7 @@ class CrawlerTask extends Task {
   Future run() async {
     logger.info("Running the crawler task");
 
-    Map feeds =
-      await firebase.get(Uri.parse('$firebaseUrl/feeds.json'));
+    Map feeds = await firebase.get(Uri.parse('$firebaseUrl/feeds.json'));
 
     List feedsAsList = [];
 
@@ -118,43 +116,40 @@ class CrawlerTask extends Task {
               ..priority = priority;
 
             // TODO: Use firebase_io lib in ItemModel?
-            var itemId = await Item.add(feed.communities.keys, newsItem.toJson(),
-                config['datastore']['firebaseSecret']);
+            var itemId = await Item.add(feed.communities.keys,
+                newsItem.toJson(), config['datastore']['firebaseSecret']);
 
             newsItem.id = itemId;
 
             UriPreview uriPreview =
-            new UriPreview(uri: Uri.parse(newsItem.url));
+                new UriPreview(uri: Uri.parse(newsItem.url));
 
             uriPreview
               ..uri = Uri.parse(newsItem.url)
               ..title = newsItem.subject
-              ..teaser =
-              InputFormatter.createIntelligentTeaser(newsItem.body);
+              ..teaser = InputFormatter.createIntelligentTeaser(newsItem.body);
 
             // Visit the URL of this item and get the best image from its page.
             var content = await http.get(feedItem.link);
             ImageInfo imageInfo =
-            await crawler.getBestImageFromHtml(content.body);
+                await crawler.getBestImageFromHtml(content.body);
 
             if (imageInfo != null) {
               // Download the image locally to our temporary file.
-              File imageFile = await downloadFileTo(
-                  imageInfo.url,
-                  await createTemporaryFile(
-                      suffix: '.' + imageInfo.extension));
+              File imageFile = await downloadFileTo(imageInfo.url,
+                  await createTemporaryFile(suffix: '.' + imageInfo.extension));
 
               var imageUtil = new ImageUtil();
               File croppedFile =
-              await imageUtil.resize(imageFile, width: 245, height: 120);
+                  await imageUtil.resize(imageFile, width: 245, height: 120);
 
               var extension = imageInfo.extension;
               var gsBucket = config['google']['cloudStorage']['bucket'];
               var gsPath = 'public/images/item/$itemId';
 
               var filename = 'main-photo.$extension';
-              var cloudStorageResponse = await app.cloudStorageUtil
-                  .uploadFile(croppedFile.path, gsBucket, '$gsPath/$filename',
+              var cloudStorageResponse = await app.cloudStorageUtil.uploadFile(
+                  croppedFile.path, gsBucket, '$gsPath/$filename',
                   public: true);
 
               uriPreview
@@ -177,9 +172,8 @@ class CrawlerTask extends Task {
               logger.severe('Error posting preview to db', error, stack);
             }
 
-            var uriPreviewId = uriPreviewResponse != null
-                ? uriPreviewResponse['name']
-                : null;
+            var uriPreviewId =
+                uriPreviewResponse != null ? uriPreviewResponse['name'] : null;
 
             Item.update(newsItem.id, {'uriPreviewId': uriPreviewId},
                 config['datastore']['firebaseSecret']);
@@ -188,10 +182,10 @@ class CrawlerTask extends Task {
           logger.severe(
               'Error while processing feed item in run()', error, stack);
         }
-      };
+      }
+      ;
 
-      if (count > 0) logger
-          .info('Retrieved $count new items from $feedUrl');
+      if (count > 0) logger.info('Retrieved $count new items from $feedUrl');
     }).catchError((error, stack) {
       logger.severe('Unhandled exception crawling feeds', error, stack);
     });
@@ -207,18 +201,16 @@ class CrawlerTask extends Task {
     metaTags.forEach((Element metaTag) {
       var property = metaTag.attributes['property'];
       if (property == 'og:title') preview.title = metaTag.attributes['content'];
-      if (property == 'og:description') preview.teaser =
-          metaTag.attributes['content'];
-      if (property == 'og:image') preview.imageOriginalUrl =
-          metaTag.attributes['content'];
+      if (property == 'og:description')
+        preview.teaser = metaTag.attributes['content'];
+      if (property == 'og:image')
+        preview.imageOriginalUrl = metaTag.attributes['content'];
 
-      if (metaTag.attributes['name'] == 'description' &&
-          preview.teaser == null) preview.teaser =
-          metaTag.attributes['content'];
+      if (metaTag.attributes['name'] == 'description' && preview.teaser == null)
+        preview.teaser = metaTag.attributes['content'];
     });
 
-    if (preview.title == null &&
-        document.querySelector('title') != null) preview.title =
-        document.querySelector('title').innerHtml;
+    if (preview.title == null && document.querySelector('title') != null)
+      preview.title = document.querySelector('title').innerHtml;
   }
 }
